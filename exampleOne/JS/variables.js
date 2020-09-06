9d0462ddacb9e5b36b280f48800037d23065fb19
navoy98 <andresnavarro_98@hotmail.com>
2020-09-06 12:17:06 -0500

    add exampleOne folder

---
+++ exampleOne/JS/variables.js
@@ -0,0 +1,12 @@
+//Scope
+let nombre = 'Andrés';
+let edad = 35;
+estatura = 1.75;
+const fechaEspecial = "20-10-2020";
+const estado_casado = true;
+//var`
+ 
+//Tu nombre es ' + nombre + ' y tu edad es ' + edad
+
+//literar string
+document.write(`Tu nomnbre es ${nombre} y tu edad es ${edad}`);
